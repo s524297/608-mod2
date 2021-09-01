d099dbb446219f262810d81f94ea7e5d7df28fc5
s524297 <89221839+s524297@users.noreply.github.com>
2021-08-31 19:53:22 -0500

    Add files via upload

--- central-native.py
+++ central-native.py
@@ -1,1 +1,12 @@
-
+total = 0 
+value_counter = 0 
+values= sorted([47, 95, 88, 73, 88, 84])
+values
+for value in values: 
+    total += value
+    value_counter += 1
+print('1. Count:', value_counter)
+print('2. Sum:', total)
+print('3. Mean:', total/value_counter)
+print('4. Meadian:', ((84+88)/2))
+print('5. Mode_most_of_listValue:', 88 )
--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,11 @@
-
+total = 0 
+value_counter = 0
+values = sorted([47, 95, 88, 73, 88, 84])
+values
+[47, 73, 84, 88, 88, 95]
+import statistics 
+print('1. Count: ', len(values))
+print('2. Sum:', sum(values))
+print('3. Mean:', statistics.mean(values))
+print('4. Mode:', statistics.mode(values))
+print('5. meadian:', statistics.median(values))
